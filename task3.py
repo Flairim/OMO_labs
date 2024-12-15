be9bb260e8c151b20a6af764f1eba2c8326ae370
Олексій Тригуб <flairim.oll@gmail.com>
2024-12-15 23:57:39 +0200

    6

--- task3.py
+++ task3.py
@@ -9,19 +9,17 @@ def g(x):
     return -np.sqrt(1-5*np.sin(x))
 
 
-x0 = -2
+x0 = -2.5
 
 def apriori_iteration_estimate(q):
 
     return  np.floor(np.log(abs( (g(x0) - x0) / (1 - q) / eps))/ np.log(1/q)) + 1
     
-def itersimple_iteration(f, g, x0, eps):
+def itersimple_iteration(f, g, x0):
 
     apriori_estimate = apriori_iteration_estimate(q)
     iter_count = 0
     x_prev = x0
-    stop_iter = 0
-    stop_x = 0
 
     print("Метод простої ітерації(Апріорна оцінка):")
     print(f"Ітерація {iter_count}: x = {x_prev}, f(x) = {f(x_prev)}")
@@ -30,11 +28,6 @@ def itersimple_iteration(f, g, x0, eps):
         iter_count += 1
         x_next = g(x_prev)
         print(f"Ітерація {iter_count}: x = {x_next}, f(x) = {f(x_next)}")
-
-        
-        if stop_iter == 0 and stop_x == 0 and abs(x_next - x_prev) < ((1-q)/q)*eps:
-            stop_iter = iter_count
-            stop_x = x_next
         
         x_prev = x_next
 
@@ -56,10 +49,10 @@ def simple_iteration(f, g, x0, eps, max_iter = 100):
         
         x_prev = x_next
 
-q = 0.05
+q = 0.7
 eps = 0.0001
 
-itersimple_iteration(f, g, x0, eps)
+itersimple_iteration(f, g, x0)
 simple_iteration(f, g, x0, eps)
 
 
@@ -79,7 +72,7 @@ def iterrelaxation_method_(x0,tau, z0, q):
         print(f"Ітерація {iter_count}: x = {x1}, f(x) = {f(x1)}")
         x0 = x1
 
-def relaxation_method_(x0,tau, z0, q, max_iter = 100):
+def relaxation_method_(x0,tau, max_iter = 100):
     iter_count = 0
 
     print("\nМетод релаксації(Апостеріорна оцінка):")
@@ -95,12 +88,14 @@ def relaxation_method_(x0,tau, z0, q, max_iter = 100):
 
         x0 = x1
 
-m1 = 6.08073
-M1 = 10.94996
+m1 = 2.92926
+M1 = 7.93646
 z0 = x0 + 2.22727
 q1 = (M1 - m1) / (M1 + m1)
 tau_opt = 2 / (m1 + M1)
-
+print(f"tau_opt = {tau_opt}")
+print(f"q1 = {q1}")
+print(f"z0 = {z0}")
 
 iterrelaxation_method_(x0, tau_opt, z0, q1)
-relaxation_method_(x0, tau_opt, z0, q1)
+relaxation_method_(x0, tau_opt)
